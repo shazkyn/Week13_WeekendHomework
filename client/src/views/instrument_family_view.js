5feb08ca00c439afb737f81899fd405ddb34c7b3
Sharon Kynaston <desmogal@gmail.com>
2018-10-26 15:43:45 +0100

    files refactored

---
+++ client/src/views/instrument_family_view.js
@@ -0,0 +1,85 @@
+import PubSub from '../helpers/pub_sub.js';
+
+class InstrumentFamilyView {
+  constructor(container){
+  this.container = container;
+  }
+// const InstrumentFamilyView = function (container) {
+//   this.container = container;
+// };
+
+  bindEvents(){
+    PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) => {
+      const instrumentFamily = evt.detail;
+      this.render(instrumentFamily);
+    });
+
+// InstrumentFamilyView.prototype.bindEvents = function () {
+//   PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) => {
+//     const instrumentFamily = evt.detail;
+//     this.render(instrumentFamily);
+//   });
+// };
+
+  }
+
+    render({name, instruments, description }) {
+    this.container.innerHTML = '';
+// InstrumentFamilyView.prototype.render = function (family) {
+//   this.container.innerHTML = '';
+  
+    const familyName = this.createElement('h2', name);
+    this.container.appendChild(familyName);
+//   const familyName = this.createElement('h2', family.name);
+//   this.container.appendChild(familyName);
+  
+    const familyDescription = this.createElement('p', description);
+    this.container.appendChild(familyDescription);
+//   const familyDescription = this.createElement('p', family.description);
+//   this.container.appendChild(familyDescription);
+  
+    const instrumentListTitle = this.createElement('h3', 'Instruments include:');
+    this.container.appendChild(instrumentListTitle);
+//   const instrumentListTitle = this.createElement('h3', 'Instruments include:');
+//   this.container.appendChild(instrumentListTitle);  
+    const instrumentList = this.createInstrumentList(instruments);
+    this.container.appendChild(instrumentList);
+//   const instrumentList = this.createInstrumentList(family.instruments);
+//   this.container.appendChild(instrumentList);
+// };
+  };
+  
+    createElement(elementType, text) {
+    const element = document.createElement(elementType);
+    element.textContent = text;
+    return element;
+  };
+// InstrumentFamilyView.prototype.createElement = function (elementType, text) {
+//   const element = document.createElement(elementType);
+//   element.textContent = text;
+//   return element;
+// };
+  createInstrumentList(instruments) {
+    const list = document.createElement('ul');
+// InstrumentFamilyView.prototype.createInstrumentList = function (instruments) {
+//   const list = document.createElement('ul');
+
+    instruments.forEach((instrument) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = instrument;
+      list.appendChild(listItem);
+    });
+
+//   instruments.forEach((instrument) => {
+//     const listItem = document.createElement('li');
+//     listItem.textContent = instrument;
+//     list.appendChild(listItem);
+//   });
+
+    return list;
+  };
+  
+}
+
+export default InstrumentFamilyView;
+// module.exports = InstrumentFamilyView;
